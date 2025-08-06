e15d1c7bc55ac67e23e8dd7a522e1a867923e05b
Jordan <jordanmacias231@gmail.com>
2025-08-06 10:14:52 -0600

    [ADD] pages

---
+++ src/app/product/page.jsx
@@ -0,0 +1,10 @@
+const ProductPage = () => {
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <p>Welcome to the product page. Here you can find various products.</p>
+    </div>
+  );
+};
+
+export default ProductPage;
---
+++ src/support/page.jsx
@@ -0,0 +1,10 @@
+const SupportPage = () => {
+  return (
+    <div>
+      <h1>Support Page</h1>
+      <p>If you need help, please contact our support team.</p>
+    </div>
+  );
+};
+
+export default SupportPage;
